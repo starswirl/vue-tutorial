a355456dfc036b2dc74276db248c03bc2ee0029a
starswirl <kzfmhpvomo@gmail.com>
2022-04-19 13:23:08 +0000

    ページを編集する

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestCase/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestCase from './components/TestCase.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestCase
   }
 }
 </script>
@@ -19,8 +18,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TestCase.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestCase',
+}
+</script>
+
+<style scoped>
+
+</style>
