19998842249914fb5dcbe15ab86bbe7d48d2220d
starswirl <kzfmhpvomo@gmail.com>
2022-04-20 07:46:54 +0000

    フォームの作成

--- src/components/TestCase.vue
+++ src/components/TestCase.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     test
+    <InputForm/>
   </div>
 </template>
 
 <script>
+import InputForm from './atoms/InputForm.vue'
 export default {
   name: 'TestCase',
+  components:{
+    InputForm
+  },
 }
 </script>
 
---
+++ src/components/atoms/InputForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="displayBox">
+    <input class="input" v-model="value">
+    <p >{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+    data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .displayBox {
+    display: flex;
+  }
+</style>
